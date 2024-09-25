5dd4ee5783cb8c57aa069bab1b21e2d23d5d4598
René Adler <rene.adler@uni-jena.de>
2024-09-25 14:57:00 +0200

    remove unused imports

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,6 @@
 #include <WiFi.h>
 #include <atomic>
 #include <bitset>
-#include <utility>
-#include <vector>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
