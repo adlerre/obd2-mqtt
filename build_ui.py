d4cf580c3d2158ab60fa56719e37f4c142875a61
René Adler <rene.adler@uni-jena.de>
2024-10-16 07:27:16 +0200

    enable verbose output

--- build_ui.py
+++ build_ui.py
@@ -36,13 +36,13 @@ def build_ui():
         try:
             if platform.system() == "Windows":
                 print(run(["npm.cmd", "install"], check=True, capture_output=True, text=True).stdout)
-                build = run(["npm.cmd", "run", "build"], check=True, capture_output=True, text=True)
+                build = run(["npm.cmd", "run", "build", "--verbose"], check=True, capture_output=True, text=True)
                 print(build.stdout)
                 if (build.stderr):
                     print(build.stderr);
             else:
                 print(run(["npm", "install"], check=True, capture_output=True, text=True).stdout)
-                build = run(["npm", "run", "build"], check=True, capture_output=True, text=True)
+                build = run(["npm", "run", "build", "--verbose"], check=True, capture_output=True, text=True)
                 print(build.stdout)
                 if (build.stderr):
                     print(build.stderr);
